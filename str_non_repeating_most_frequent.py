44a07004e3c1b03bc0fbf35c84b8059c31d998d3
Jayesh Bhandarkar <bhandarkarjayesh721@gmail.com>
2025-07-12 23:16:47 +0530

    Update str_non_repeating_most_frequent.py

--- str_non_repeating_most_frequent.py
+++ str_non_repeating_most_frequent.py
@@ -1,17 +1,3 @@
-"""
-Given a string S, find:
-1. The first non-repeating character in S (or "None" if none exists).
-2. The most frequent character (or the first character if all are unique).
-
-If the string is empty then print invalid input 
-
-Input 1: swadesh
-Output 1: w s
-
-Input 2: aabbcc
-Output 2: None a
-"""
-
 def find_chars(s):
     if not s:  
         print("Invalid input")
