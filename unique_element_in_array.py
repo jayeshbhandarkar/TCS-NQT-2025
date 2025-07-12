f74bcc5190d4cc94a8dabe2f86ab77b8fdcdeea3
Jayesh Bhandarkar <bhandarkarjayesh721@gmail.com>
2025-07-12 23:12:16 +0530

    Update unique_element_in_array.py

--- unique_element_in_array.py
+++ unique_element_in_array.py
@@ -14,15 +14,18 @@ arr = list(map(int, input().strip("[]").split(",")))
 print(find_unique(arr))
 
 
-# def find_unique(arr):
-#     for i in arr:
-#         if arr.count(i) == 1:
-#             return i
-#         return None
+"""
+def find_unique(arr):
+    for i in arr:
+        if arr.count(i) == 1:
+            return i
+        return None
 
-# arr = list(map(int, input().strip("[]").split(",")))
-# print(find_unique(arr))
+arr = list(map(int, input().strip("[]").split(",")))
+print(find_unique(arr))
+"""
 
+# ---------------------------------------------------
 
 """
 n = int(input())
@@ -32,26 +35,4 @@ unique_element = 0
 for num in arr:
     unique_element ^= num
 print(unique_element)
-"""
-
-
-# If we want to return all the unique elements from the array
-"""
-def find_unique(arr):
-    count = {}
-    for i in arr:
-        if i in count:
-            count[i] += 1
-        else:
-            count[i] = 1
-    
-    result = []
-    for i in count:
-        if count[i] == 1:
-            result.append(i)
-        
-    return result
-    
-arr = list(map(int, input().strip("[]").split(",")))
-print(find_unique(arr))
 """
